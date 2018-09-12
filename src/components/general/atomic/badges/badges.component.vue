a08952b2d5517c60e5b8efe11d892f8252857871
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-09-12 23:29:26 +0700

    Revert "Feature: create image component & badge test case"

--- src/components/general/atomic/badges/badges.component.vue
+++ src/components/general/atomic/badges/badges.component.vue
@@ -1,64 +1,7 @@
 <template>
-  <div :class="classes">
-    <div class="ef-badges__content">
-      <slot />
-    </div>
-    <transition
-      :css="false"
-      name="slide-up-fade"
-      @before-enter="beforeEnter"
-      @enter="enter"
-      @leave="leave"
-    >
-      <div
-        v-if="value"
-        class="ef-badges__badges">
-        <slot name="badge" />
-      </div>
-    </transition>
-  </div>
+  <h1>test</h1>
 </template>
 
 <style lang="scss">
-@import '@/style/scss/import.scss';
-@import './style.scss';
+@import 'style.scss';
 </style>
-
-<script>
-import props from './prop-types'
-
-export default {
-  props,
-  computed: {
-    classes() {
-      return {
-        'ef-badges': true,
-        'ef-badges--left': this.position === 'left',
-        'ef-badges--right': this.position === 'right',
-        [`ef-badges--color-${this.color}`]: this.color != null
-      }
-    }
-  },
-  methods: {
-    beforeEnter(el) {
-      el.style.opacity = 0
-
-      return el
-    },
-    enter /* istanbul ignore next */(el, done) {
-      Velocity(el, { opacity: 0.75 }, { duration: 300 })
-      Velocity(el, { opacity: 1 }, { complete: done })
-    },
-    leave /* istanbul ignore next */(el, done) {
-      Velocity(
-        el,
-        {
-          translateY: '10px',
-          opacity: 0
-        },
-        { complete: done }
-      )
-    }
-  }
-}
-</script>
